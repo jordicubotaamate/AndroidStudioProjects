aa78d4982e205ff394983bf752d65f20222000fa
Jordi Cubota Amate <cubota.jordi.inf2@gmail.com>
2015-12-24 13:41:26 +0100

    Inici Random Test

---
+++ Infoserveis/app/src/main/java/cat/infoserveis/jordi/infoserveis/MainActivity.java
@@ -0,0 +1,73 @@
+package cat.infoserveis.jordi.infoserveis;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "jordi@infoserveis.cat", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        final Random rnd = new Random();
+
+        final EditText num1 = (EditText) findViewById(R.id.num1);
+        final EditText num2 = (EditText) findViewById(R.id.num2);
+        Button genera = (Button) findViewById(R.id.button1);
+        final TextView random = (TextView) findViewById(R.id.numeroRandom);
+        genera.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int min = Integer.parseInt(num1.getText().toString());
+                int max = Integer.parseInt(num2.getText().toString());
+                int rndInt = min+rnd.nextInt(max-min)+1;
+                random.setText(Integer.toString(rndInt));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
